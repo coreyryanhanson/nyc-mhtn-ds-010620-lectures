4f95367e427302d73a5d0d58f71c3006468402ea
Corey <17171838+coreyryanhanson@users.noreply.github.com>
2020-01-10 18:11:32 -0500

    added function to calculate iqr

--- week_1/Calculator_class/descriptive.py
+++ week_1/Calculator_class/descriptive.py
@@ -47,6 +47,37 @@ class Calculator:
     def stand_dev(self):
         return math.sqrt(self.variance)
 
+    @property
+    def quartiles(self):
+        ordered = sorted(self.dataset)
+        length = self.length
+        quart_odd = lambda x, y: [ordered[x], ordered[y]]
+        quart_even = lambda x1, x2, y1, y2: [(ordered[x1]+ordered[x2])/2, (ordered[y1]+ordered[y2])/2]
+        if length % 2 and (length - 1) % 4:
+            q1n = int((length - 3 ) / 4)
+            q3n = length-(q1n) - 1
+            return quart_odd(q1n, q3n)
+        if length % 2 and not (length - 1) % 4:
+            q1na = int((length - 1 ) / 4)
+            q1nb = q1na - 1
+            q3na = length - q1na - 1
+            q3nb = q3na + 1
+            return quart_even(q1na, q1nb, q3na, q3nb)
+        if not length % 2 and (length) % 4:
+            q1n = int((length - 2) / 4)
+            q3n = length-(q1n) - 1
+            return quart_odd(q1n, q3n)
+        if not length % 4:
+            q1na = int((length) / 4)
+            q1nb = q1na - 1
+            q3na = length - (q1na) - 1
+            q3nb = q3na + 1
+            return quart_even(q1na, q1nb, q3na, q3nb)
+
+    @property
+    def iqr(self):
+        return self.quartiles[1] - self.quartiles[0]
+
     def add_data(self, data):
         self.dataset.extend(data)
 
