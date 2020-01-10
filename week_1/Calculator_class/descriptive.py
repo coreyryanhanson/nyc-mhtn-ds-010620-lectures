eddd01022f145b2bca87f33114532340c8980f56
Corey <17171838+coreyryanhanson@users.noreply.github.com>
2020-01-10 11:59:50 -0500

    finished calculator

--- week_1/Calculator_class/descriptive.py
+++ week_1/Calculator_class/descriptive.py
@@ -8,5 +8,51 @@ Created on Wed Sep  4 14:25:52 2019
 import math
 
 
+
 class Calculator:
-    pass        
+    def __init__(self, data):
+        self.dataset = data
+
+    @property
+    def length(self):
+        return len(self.dataset)
+
+    @property
+    def mean(self):
+        total = 0
+        for data in self.dataset:
+            total += data
+        return total / len(self.dataset)
+
+    @property
+    def median(self):
+        ordered = sorted(self.dataset)
+        length = self.length
+        if length % 2:
+            n = int((length - 1) / 2)
+            return ordered[n]
+        else:
+            m1 = int(length / 2)
+            m2 = m1 - 1
+            return (ordered[m1] + ordered[m2]) / 2
+
+    @property
+    def variance(self):
+        total = 0
+        for datum in self.dataset:
+            total += (datum - self.mean) ** 2
+        return total / (len(self.dataset) - 1)
+
+    @property
+    def stand_dev(self):
+        return math.sqrt(self.variance)
+
+    def add_data(self, data):
+        self.dataset.extend(data)
+
+    def remove_data(self, deletions):
+        new_list = []
+        for data in self.dataset:
+            if data not in deletions:
+                new_list.append(data)
+        self.dataset = new_list
