e958cf9d61febeb5c222ea3b10c1e0ba963792ff
Corey <corey@localhost.localdomain>
2020-01-12 14:33:18 -0500

    fixed typo

--- week_1/Calculator_class/descriptive.py
+++ week_1/Calculator_class/descriptive.py
@@ -102,7 +102,7 @@ class Calculator:
                 the_mode.append(data)
             else:
                 continue
-        if self.dataset == the_mode:
+        if ordered == the_mode:
             print("There is no mode")
             the_mode = None
         return the_mode
