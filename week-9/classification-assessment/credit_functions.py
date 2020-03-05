2934fc8a1bbb18005bf0f0468a3b8c4f8497bf7a
Corey <corey@localhost.localdomain>
2020-03-04 22:49:14 -0500

    fixed objects

--- week-9/classification-assessment/credit_functions.py
+++ week-9/classification-assessment/credit_functions.py
@@ -8,17 +8,18 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import pickle
 
 class ModelSwitcher(object):
-    def __init__(self, df, target, selection):
+    def __init__(self, df, target, selection, balance_class=False, scale_type=False):
+        self.df = df
         self.random_state = 1
         self.test_size = .2
+        self.balance_class = balance_class
+        self.scale_type = scale_type
         self.selection = selection
         self.target = target
         self.X = df[selection]
         self.y = df[target]
-        self._train_test_split()
-        self._check_balance()
-        self._scale_setter()
-        self._scale_getter()
+        self._data_preprocessing()
+
 
     def _train_test_split(self):
         X, y = self.X, self.y
@@ -26,34 +27,58 @@ class ModelSwitcher(object):
         self.X_train, self.X_test, self.y_train, self.y_test = X_train, X_test, y_train, y_test
 
     def _scale_setter(self):
-        self.scaler = StandardScaler()
+        if self.scale_type == "standard":
+            print("Using standard scaler")
+            self.scaler = StandardScaler()
+        elif self.scale_type == "minmax":
+            print("Using Min/Max scaler")
+            self.scaler = MinMaxScaler()
+        else:
+            print("No scaling specified")
+            self.scale_type = False
+            return
         self.scaler.fit(self.X_train)
 
     def _scale_getter(self):
+        if self.scale_type == False:
+            print("Skipping scaling")
+            return
         self.X_train = self.scaler.transform(self.X_train)
         self.X_test = self.scaler.transform(self.X_test)
 
-    def _check_balance(self):
-        new_X, new_y = self._class_imbalance(self.target, 0, 1)
-        self.X_train, self.y_train = new_X, new_y
+    def _data_preprocessing(self):
+        self._train_test_split()
+        self._class_imbalance(0, 1)
+        self._scale_setter()
+        self._scale_getter()
 
-    def _class_imbalance(self, target, majority_val, minority_val):
+    def _class_imbalance(self, majority_val, minority_val):
+        target = self.target
         df = pd.concat([self.X_train, self.y_train], axis=1)
         majority, minority = df[df[target] == majority_val], df[df[target] == minority_val]
-        simp_upsample = self._simple_resample(minority, majority)
-        simp_downsample = self._simple_resample(majority, minority)
-        smote_x, smote_y = self._smote_data(self.X_train, self.y_train)
-        up_y, up_X = simp_upsample[target], simp_upsample.drop(target, axis=1)
-        return up_X, up_y
+        if self.balance_class == "upsample":
+            print("Performing upsample")
+            self._simple_resample(minority, majority)
+        elif self.balance_class == "downsample":
+            print("Performing downsample")
+            self._simple_resample(majority, minority)
+        elif self.balance_class == "smote":
+            print("Performing smote")
+            self._smote_data()
+        else:
+            print("Skipping class imbalance functions")
 
     def _simple_resample(self, to_change, goal):
         resampled = resample(to_change, replace=True, n_samples=len(goal), random_state=self.random_state)
-        return pd.concat([goal, resampled])
+        joined = pd.concat([goal, resampled])
+        self.X_train, self.y_train = joined.drop(self.target, axis=1), joined[self.target]
 
-    def _smote_data(self, X_train, y_train):
+    def _smote_data(self):
         sm = SMOTE(sampling_strategy=1.0, random_state=self.random_state)
-        sm.fit_sample(X_train, y_train)
-        return X_train, y_train
+        self.X_train, self.y_train = sm.fit_sample(self.X_train, self.y_train)
+
+    def column_drop(self, columns):
+        self.selection = self.selection.drop(labels=columns)
 
 
 def pickle_read(path):
